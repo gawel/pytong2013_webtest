1ed945cc9f958fd0dfc5c00061af2007d59cbf99
Gael Pasgrimaud <gael@gawel.org>
2013-06-21 13:25:11 +0200

    path

--- build_docs.sh
+++ build_docs.sh
@@ -3,7 +3,7 @@ git clone git@github.com:gawel/pytong2013_webtest.git pages
 cd pages
 git checkout gh-pages
 rm index.html
-../bin/impress -i ../docs/index.rst -o .
+../bin/impress -i index.rst -o .
 git add -A
 git commit -m "update docs"
 git push origin gh-pages
