ac2dc86f6aa81e5c16a4809fd794621cdfd19718
Gael Pasgrimaud <gael@gawel.org>
2014-07-07 14:57:20 +0200

    one more step

--- build_docs.sh
+++ build_docs.sh
@@ -2,8 +2,8 @@
 git clone git@github.com:gawel/webtest-2013.git pages
 cd pages
 git checkout gh-pages
-rm index.html
-../bin/impress -i ../docs/index.rst -o .
+rm -f index.html
+../bin/impress -i ../index.rst -o .
 git add -A
 git commit -m "update docs"
 git push origin gh-pages
